60e120e87c524dc5ceb55d7018c1bd09f0582d88
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-09 10:48:57 -0400

    Sget working...Math.max is only displaying the highest number

--- assessment1_3_9.js
+++ assessment1_3_9.js
@@ -3,22 +3,19 @@ Create an interactive program which allows a user to enter (3) numbers,
 determines the order of the numbers from largest to smallest, and displays the result.
 */
 
+start();
+
 var sget=require('sget');
 var num1=Number(sget("Please enter your first number"));
 var num2=Number(sget("Please enter your second number"));
 var num3=Number(sget("Please enter your third number"));
 
-start();
-compareNumbers();
-
 function start(){
 	console.log("Welcome! You will enter 3 numbers which will be displayed from largest to smallest")
-	console.log(num1);
-	console.log(num2);
-	console.log(num3);
+
 }
 
 function compareNumbers(){
 	console.log(Math.max(num1,num2,num3));
 }
-
+compareNumbers();
