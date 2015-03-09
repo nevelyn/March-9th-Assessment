9497e0b1aa372964d7d91bb38a13d84f2c3f9b0d
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-09 10:57:40 -0400

    Setting up functions and sget for user input

--- assessment2_3_9.js
+++ assessment2_3_9.js
@@ -4,3 +4,15 @@ Create an interactive program which allows a user to create a password.
 If the password is at least 10 characters long AND contains at least (1) capital letter 
 OR an exclamation point, display a confirmation message; otherwise, display an error.
 */
+var sget=require('sget');
+var password=sget('');
+
+function start(){
+	console.log("Please create a password that meets the following requirements\n1.Must be at least 10 characters long \n2. Must contain at least (1) capital letter -or- exclamation point");
+}
+start();
+function evaluatePassword(){
+	if (password.length<9){
+		console.log("Your password is too short.")
+	}
+}
