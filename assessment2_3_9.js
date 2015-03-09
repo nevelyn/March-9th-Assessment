c03abc21b9910b46174d4b1708670d10105c5e94
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-09 11:21:19 -0400

    Updating functions and trying to debug

--- assessment2_3_9.js
+++ assessment2_3_9.js
@@ -8,11 +8,19 @@ var sget=require('sget');
 var password=sget('');
 
 function start(){
-	console.log("Please create a password that meets the following requirements\n1.Must be at least 10 characters long \n2. Must contain at least (1) capital letter -or- exclamation point");
+	console.log("Please create a password that meets the following requirements\n1.Must be at least 10 characters long \n2. Must contain at least (1) capital letter -or- (1) exclamation point");
 }
 start();
 function evaluatePassword(){
-	if (password.length<9){
-		console.log("Your password is too short.")
-	}
+	
+	if(password.length>=9 && password.indexOf(toUpperCase)|| password.indexOf('!') === 1){
+		console.log("Success! Your password has been accepted!");
+
+	}else if(password.indexOf(toUpperCase) || password.indexOf('!') === -1){
+		console.log("Your password requires a capital letter or an exclamation point.");
+
+	}else(password.length<9){
+		console.log("Your is too short.");
+	} 
 }
+evaluatePassword();
