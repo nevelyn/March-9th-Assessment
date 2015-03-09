7c2bf1e1bf74ccf67964504c0691cf6d8e9ea2fd
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-09 09:43:17 -0400

    Creating new files

---
+++ assessment1_3_9.js
@@ -0,0 +1,5 @@
+/*1. Input & Output, Control Flow
+Create an interactive program which allows a user to enter (3) numbers,
+determines the order of the numbers from largest to smallest, and displays the result.
+*/
+
